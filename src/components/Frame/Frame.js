9065f94ef34c64f43e9d410b45d9886e46d00486
Gerhard Bliedung <gerhard@interactivethings.com>
2016-05-31 17:24:00 +0200

    adapt the Frame scaling accordingly

--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -8,27 +8,29 @@ export default class Frame extends Component {
   }
 
   render() {
-    const {children} = this.props;
+    const {children, width, parentWidth} = this.props;
     const height = this.props.height || this.state.height;
     const autoHeight = this.props.height === void 0;
 
     const style = {
-      width: '100%',
+      width: width || '100%',
       lineHeight: 0,
       margin: 0,
       padding: 0,
       border: 'none',
-      height
+      transformOrigin: '0% 50%',
+      height: height,
+      transform: `scale( ${width <=  parentWidth ? 1 : parentWidth / width} )`
     };
 
     return (
-      <div style={{lineHeight: 0}}>
+      <div style={{lineHeight: 0, width: parentWidth}} ref='container'>
         <FrameComponent
           style={style}
           frameBorder='0'
           allowTransparency='true'
           scrolling='no'
-          head={<style>{'html,body{margin:0;padding:0;}'}</style>}
+          head={<style>{'html,body{margin:0;padding:0}'}</style>}
           onRender={autoHeight ? (content) => {
             const contentHeight = content.offsetHeight;
             if (contentHeight !== height) {
@@ -45,5 +47,7 @@ export default class Frame extends Component {
 
 Frame.propTypes = {
   children: PropTypes.element,
+  width: PropTypes.number,
+  parentWidth: PropTypes.number,
   height: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 };
